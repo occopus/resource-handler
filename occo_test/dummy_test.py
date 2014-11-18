a38edd0e6f3e8d6d153da456ffb0cc03aefe168e
Adam Visegradi <visegradi.adam@sztaki.mta.hu>
2014-11-18 14:39:19 +0100

    Implement dummy drop node+node state tests

--- occo_test/dummy_test.py
+++ occo_test/dummy_test.py
@@ -15,6 +15,14 @@ class DummyTest(unittest.TestCase):
         eq_(nd['instance_id'], nid)
         ok_('running' in nd)
         ok_(nd['running'])
-
-
+    def test_node_state(self):
+        nd = dict()
+        nid = self.ch.create_node(nd)
+        eq_(self.ch.get_node_state(nid), nd)
+    def test_drop_node(self):
+        nd = dict()
+        nid = self.ch.create_node(nd)
+        self.ch.drop_node(nid)
+        s = self.ch.get_node_state(nid)
+        eq_(s['running'], False)
     
