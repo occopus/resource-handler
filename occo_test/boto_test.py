19d1c3c072836ced57804cc7ae374c7045c2d337
Adam Visegradi <visegradi.adam@sztaki.mta.hu>
2014-11-24 13:43:08 +0100

    Instantiate boto CH from code

--- occo_test/boto_test.py
+++ occo_test/boto_test.py
@@ -4,13 +4,15 @@ import unittest
 from nose.tools import ok_, eq_
 import common
 import occo.cloudhandler.backends.boto
+from occo.cloudhandler import CloudHandler
 
 class BotoTest(unittest.TestCase):
     def setUp(self):
         cfg = common.configure()
-        self.ch = cfg.clouds['boto_lpds_cloud_instance']
+        self.cfg = cfg.clouds['boto_lpds_cloud_instance']
         self.node_def = cfg.node_defs['node1']
     def test_node(self):
+        self.ch = CloudHandler(**self.cfg)
         nid = self.ch.create_node(self.node_def)
         try:
             print self.ch.get_node_state(nid)
