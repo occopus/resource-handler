26124ab3cc2b8f2047deb396e0b546740bcec388
Adam Visegradi <visegradi.adam@sztaki.mta.hu>
2014-11-24 13:44:16 +0100

    Load auth data form external configuration

--- occo_test/boto_test.py
+++ occo_test/boto_test.py
@@ -5,11 +5,14 @@ from nose.tools import ok_, eq_
 import common
 import occo.cloudhandler.backends.boto
 from occo.cloudhandler import CloudHandler
+import yaml
 
 class BotoTest(unittest.TestCase):
     def setUp(self):
         cfg = common.configure()
         self.cfg = cfg.clouds['boto_lpds_cloud_instance']
+        with open('occo_test/auth_data.yaml') as f:
+            self.cfg['auth_data'] = yaml.load(f)
         self.node_def = cfg.node_defs['node1']
     def test_full_dryrun(self):
         self.cfg['dry_run'] = True
