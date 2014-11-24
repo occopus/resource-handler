0dfecaff84dd0e3ce93fba50aedfdef5fbaeee85
Adam Visegradi <visegradi.adam@sztaki.mta.hu>
2014-11-24 14:32:33 +0100

    Implement real node-deletion test

--- occo_test/boto_test.py
+++ occo_test/boto_test.py
@@ -7,17 +7,29 @@ import occo.cloudhandler.backends.boto
 from occo.cloudhandler import CloudHandler
 import yaml
 import logging
+import os
 
 log = logging.getLogger('occo_test.boto_test')
+
+DROP_NODES_FILE = 'occo_test/drop_nodes.yaml'
 
 class BotoTest(unittest.TestCase):
     def setUp(self):
         cfg = common.configure()
+
+        if os.path.isfile(DROP_NODES_FILE):
+            with open(DROP_NODES_FILE) as f:
+                self.drop_nodes = yaml.load(f)
+        else:
+            self.drop_nodes = []
+
         self.cfg = cfg.clouds['boto_lpds_cloud_instance']
         with open('occo_test/auth_data.yaml') as f:
             self.cfg['auth_data'] = yaml.load(f)['boto_lpds']
         log.debug('Using Boto config:\n%s', yaml.dump(self.cfg))
+
         self.node_def = cfg.node_defs['node1']
+
     def test_full_dryrun(self):
         self.cfg['dry_run'] = True
         self.ch = CloudHandler(**self.cfg)
@@ -32,3 +44,20 @@ class BotoTest(unittest.TestCase):
         self.ch = CloudHandler(**self.cfg)
         nid = self.ch.create_node(self.node_def)
         log.debug("Resource acquired; node_id = '%s'", nid)
+
+    def test_drop_node(self):
+        self.cfg['dry_run'] = False
+        self.ch = CloudHandler(**self.cfg)
+        remaining = []
+        last_exception = None
+        for i in self.drop_nodes:
+            try:
+                self.ch.drop_node(i)
+            except Exception as ex:
+                log.exception('Failure:')
+                last_exception = ex
+                remaining.append(i)
+        if last_exception:
+            with open(DROP_NODES_FILE, 'w') as f:
+                f.write(yaml.dump(remaining))
+                raise last_exception
