86b8e519b3db07c08918a2e3007ae9a0e46b8d34
Adam Visegradi <visegradi.adam@sztaki.mta.hu>
2015-01-28 11:55:42 +0100

    Document occo.cloudhandler.cloudhandler

--- occo/cloudhandler/cloudhandler.py
+++ occo/cloudhandler/cloudhandler.py
@@ -18,14 +18,44 @@ log = logging.getLogger('occo.cloudhandler')
 
 class CloudHandler(factory.MultiBackend):
     """
-    One-shot objects performing a single operation. No run-time state
-    is retained.
+    Abstract interface of a Cloud Handler.
+
+    ``CloudHandler``\ s are one-shot objects performing a single operation; no
+    run-time state is retained. This enables the trivial parallelization of
+    performing Cloud Handler instructions.
+
+    .. todo:: This class will need to be an RPC subject; therefore we need:
+
+        - A stub-skeleton pair
+        - Probably the implementation of the Command strategy. But we need to
+        think this through first. (Many methods in the ``CloudHandler``
+        iterface are better supported with the Command strategy: it's much more
+        efficient. If we are reasonably sure that we'll only have these three
+        methods, we can simply proxy each of them individually.)
     """
     def __init__(self, **config):
         self.__dict__.update(config)
+
     def create_node(self, node_description):
+        """ Instantiate a node.
+
+        :param node_description: Information required to instantiate the node.
+            Its contents are specified by the sub-class.
+        """
         raise NotImplementedError()
+
     def drop_node(self, instance_data):
+        """ Destroy a node instance.
+
+        :param instance_data: Information required to destroy a node instance.
+            Its contents are specified by the sub-class.
+        """
         raise NotImplementedError()
+
     def get_node_state(self, instance_data):
+        """ Query instance information.
+
+        :param instance_data: Information required to perform the query.
+            Its contents are specified by the sub-class.
+        """
         raise NotImplementedError()
