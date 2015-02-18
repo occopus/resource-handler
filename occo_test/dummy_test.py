c64007dc48bd5d77c6e50c723913b94d4cee5f56
Novak Adam <novak.adam@sztaki.mta.hu>
2015-02-18 13:39:30 +0000

    Start revision of unittests, further revision needed

--- occo_test/dummy_test.py
+++ occo_test/dummy_test.py
@@ -4,12 +4,17 @@ import unittest
 from nose.tools import ok_, eq_
 import common
 
+##TODO: revise tests
+##	- in test_drop_node s is a string - find viable solution
 class DummyTest(unittest.TestCase):
     def setUp(self):
         cfg = common.configure()
         self.ch = cfg.clouds['dummy_cloud_instance0']
     def test_create_node(self):
         nd = dict()
+	nd['id'] = 'test_node_id'
+	nd['environment_id'] = 'test_env_id'
+	nd['name'] = 'test_name'
         nid = self.ch.create_node(nd)
         ok_('instance_id' in nd)
         eq_(nd['instance_id'], nid)
@@ -17,12 +22,22 @@ class DummyTest(unittest.TestCase):
         ok_(nd['running'])
     def test_node_state(self):
         nd = dict()
+	nd['id'] = 'test_node_id'
+	nd['environment_id'] = 'test_env_id'
+	nd['name'] = 'test_name'
         nid = self.ch.create_node(nd)
-        eq_(self.ch.get_node_state(nid), nd)
+	instance_data = dict()
+	instance_data['instance_id'] = nid
+        eq_(self.ch.get_node_state(instance_data), nd)
     def test_drop_node(self):
         nd = dict()
+	nd['id'] = 'test_node_id'
+	nd['environment_id'] = 'test_env_id'
+	nd['name'] = 'test_name'
         nid = self.ch.create_node(nd)
-        self.ch.drop_node(nid)
-        s = self.ch.get_node_state(nid)
+	instance_data = dict()
+	instance_data['instance_id'] = nid
+        self.ch.drop_node(instance_data)
+        s = self.ch.get_node_state(instance_data)
         eq_(s['running'], False)
     
