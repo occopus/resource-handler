9de6a9d9a72f87b901f3d5fc51fed3b57558f15b
Novak Adam <novak.adam@sztaki.mta.hu>
2015-02-20 13:03:03 +0000

    fix dummy_test.py

--- occo_test/dummy_test.py
+++ occo_test/dummy_test.py
@@ -12,32 +12,32 @@ class DummyTest(unittest.TestCase):
         self.ch = cfg.clouds['dummy_cloud_instance0']
     def test_create_node(self):
         nd = dict()
-	nd['id'] = 'test_node_id'
-	nd['environment_id'] = 'test_env_id'
-	nd['name'] = 'test_name'
+        nd['id'] = 'test_node_id'
+        nd['environment_id'] = 'test_env_id'
+        nd['name'] = 'test_name'
         nid = self.ch.create_node(nd)
-        ok_('instance_id' in nd)
-        eq_(nd['instance_id'], nid)
-        ok_('running' in nd)
-        ok_(nd['running'])
+        self.assertIsNotNone(nid)
+        self.assertIn(nid, self.ch.kvstore)
+        self.assertIn('running', self.ch.kvstore[nid])
+        self.assertTrue(self.ch.kvstore[nid]['running'])
     def test_node_state(self):
         nd = dict()
-	nd['id'] = 'test_node_id'
-	nd['environment_id'] = 'test_env_id'
-	nd['name'] = 'test_name'
+        nd['id'] = 'test_node_id'
+        nd['environment_id'] = 'test_env_id'
+        nd['name'] = 'test_name'
         nid = self.ch.create_node(nd)
-	instance_data = dict()
-	instance_data['instance_id'] = nid
+        instance_data = dict()
+        instance_data['instance_id'] = nid
         eq_(self.ch.get_node_state(instance_data), 'running')
     def test_drop_node(self):
         nd = dict()
-	nd['id'] = 'test_node_id'
-	nd['environment_id'] = 'test_env_id'
-	nd['name'] = 'test_name'
+        nd['id'] = 'test_node_id'
+        nd['environment_id'] = 'test_env_id'
+        nd['name'] = 'test_name'
         nid = self.ch.create_node(nd)
-	instance_data = dict()
-	instance_data['instance_id'] = nid
+        instance_data = dict()
+        instance_data['instance_id'] = nid
         self.ch.drop_node(instance_data)
         s = self.ch.get_node_state(instance_data)
         eq_(s, 'unknown')
-    
+
