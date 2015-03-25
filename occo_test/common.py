dce05a104e99363f3d6f04c19eee4bbb9bd7466f
Adam Visegradi <visegradi.adam@sztaki.mta.hu>
2015-03-25 16:24:36 +0100

    Fixup tests for change in !yaml_import

--- occo_test/common.py
+++ occo_test/common.py
@@ -1,6 +1,7 @@
 #!/dev/null
 
 import logging.config
+import occo.util as util
 import occo.util.config as config
 import occo.infobroker.kvstore
 import occo.cloudhandler.backends.dummy
@@ -10,6 +11,7 @@ cfg = object()
 
 def configure():
     global cfg
+    util.set_config_base_dir('etc/occo')
     with open('occo_test/test.yaml') as f:
         cfg = config.DefaultYAMLConfig(f)
     logging.config.dictConfig(cfg.logging)
