0d034b7418f7d5b827d9db37ec528d5b8798e92d
Adam Visegradi <visegradi.adam@sztaki.mta.hu>
2015-08-25 10:14:42 +0000

    Add script that recreates the virtualenv

---
+++ reset-env.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -ex
+
+PDIR=env/ch-test
+
+echo "Reseting '$PDIR'"
+
+rm -rf "$PDIR"
+
+virtualenv --no-site-packages "$PDIR"
+source "$PDIR"/bin/activate
+pip install --no-deps -r requirements_test.txt
+
+set +ex
+echo "It's dangerous to go alone. Take these:"
+echo "source '$PDIR/bin/activate'"
+echo "nosetests"
