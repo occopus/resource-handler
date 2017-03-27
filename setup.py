e0264643f573bab2a1855af2d1b9cea13281f0e8
Jozsef Kovacs <jozsef.kovacs@sztaki.mta.hu>
2017-03-27 15:26:00 +0200

    Prepare for release 1.4

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from pip.req import parse_requirements
 
 setuptools.setup(
     name='OCCO-ResourceHandler',
-    version='1.3',
+    version='1.4',
     author='MTA SZTAKI',
     author_email='occopus@lpds.sztaki.hu',
     namespace_packages=[
